0afdba4a2a9c912bf2079491517491dcb9907fd7
Andrej Podhradsky <apodhrad@redhat.com>
2014-04-28 21:49:18 +0200

    Init activemq-client

---
+++ src/main/java/org/apodhrad/example/activemq/client/Client.java
@@ -0,0 +1,49 @@
+package org.apodhrad.example.activemq.client;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Client {
+
+	public static void main(String[] args) throws JMSException {
+		String text = "Hello ActiveMQ!";
+        if(args.length > 0) {
+        	text = args[0];
+        }
+		
+		// Create a ConnectionFactory
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+        // Create a Connection
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        // Create a Session
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // Create the destination (Topic or Queue)
+        Destination destination = session.createQueue("myqueue");
+
+        // Create a MessageProducer from the Session to the Topic or Queue
+        MessageProducer producer = session.createProducer(destination);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        // Create a messages
+        TextMessage message = session.createTextMessage(text);
+
+        // Tell the producer to send the message
+        System.out.println("Sent message: "+ text);
+        producer.send(message);
+
+        // Clean up
+        session.close();
+        connection.close();
+	}
+}
